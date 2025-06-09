259232084558b470c2f830b04b1f3673d5bbf75f
duy trịnh <duytrinh25072001@gmail.com>
2025-06-09 08:56:17 +0700

    update project

---
+++ src/js/message.js
@@ -0,0 +1,14 @@
+// Bài 2:
+// function showMessage() {
+//     document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+// }
+
+// Bài 3:
+// function showMessage() {
+//     document.write("Xin chào!");
+// }
+
+// Bài 4:
+function showMessage() {
+    console.log("Xin chào!")
+}
