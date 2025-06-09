f9e1f9e048db6721182dffa2d3b8acd96d54e056
duy trịnh <duytrinh25072001@gmail.com>
2025-06-09 14:07:32 +0700

    update project

--- src/js/lesson5js/circleArea.js
+++
@@ -1,8 +0,0 @@
-function Bai3() {
-    let pi = 3.14;
-    let r = prompt("Nhập bán kính hình tròn");
-    let radius = parseInt(r);
-    let acreage = radius * pi;
-    // document.write("Diện tích hình tròn là: " + acreage);
-    result.innerHTML = `Bài 3: <br> Diện tích hình tròn là : ${acreage}`
-}
--- src/js/lesson5js/circumFerence.js
+++
@@ -1,8 +0,0 @@
-function Bai4() {
-    let pi = 3.14;
-    let r = prompt("nhập bán kính hình tròn")
-    let radius = parseInt(r);
-    let perimeter = 2 * pi * radius;
-    // document.write("Chu vi hình tròn là: " + perimeter);
-    result.innerHTML = `Bài 4: <br> Chu vi hình tròn là: ${perimeter}`
-}
