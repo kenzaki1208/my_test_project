e5ee8ba955cc4c2bb4ce687eb009677ec72e8da3
duy trịnh <duytrinh25072001@gmail.com>
2025-06-20 09:18:03 +0700

    uupdate project

--- src/lesson10/checkSymmetry/script.js
+++ src/lesson10/checkSymmetry/script.js
@@ -1,17 +1,17 @@
  function isPalindrome(str) {
-    str = str.toLowerCase();
+    str = str.toLowerCase(); // ký tự thường = ký tự in hoa
 
-    if (str === "") {
+    if (str === "") {           // nếu chuỗi bằng rỗng trả về true
         return true;
-    } else if (str.length === 1) {
+    } else if (str.length === 1) {          // nếu chuỗi bằng 1 cũng trả về true
         return true;
     }
 
-    if (str[0] === str[str.length - 1]) {
-        return isPalindrome(str.slice(1, str.length-1));
-    } else {
-        return false;
-    }
+    if (str[0] === str[str.length - 1]) {       //nếu ký tự đầu bằng ký tự cuối, ví dụ "rotor" thì r = r
+        return isPalindrome(str.slice(1, str.length-1));    //trả về so sánh những ký tự còn lại ví dụ rotor thì so sánh oto
+    } else {                                    //hàm slice sẽ trả về phần trích suất đầu và cuối,
+        return false;                           //ở đây phần trên đã kiểm tra ký tự đầu và cuối rồi thì hàm slice sẽ bắt đầu từ phần tử thứ 2 và phần tử gần cuối
+    }                                           //các trường hợp khác đều trả về false
  }
 
  let textInput = isPalindrome(prompt("Nhập vào 1 chữ bất kì: "));
